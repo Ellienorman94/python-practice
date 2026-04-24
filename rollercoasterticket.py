439ee54eb1560b6d8f35e12760cd4988c62558d8
Ellie Norman <eleanornorman9@gmail.com>
2026-04-24 11:13:31 +0100

    Adding a rollercoaster ticketing system

---
+++ rollercoasterticket.py
@@ -0,0 +1,25 @@
+print("Welcome to the Cruising Coaster!")
+height = int(input("What is your height in cm?"))
+bill = 0
+
+if height >= 120:
+    print ("You can ride the Cruising Coaster!")
+    age = int(input("How are are you?"))
+    if age <= 12:
+        bill = 5
+        print("Child tickets are £5")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are £7")
+    else:
+        bill = 12
+        print("Adult tickets are £12")
+
+    buy_photo = input("Would you like to buy a photo? Y or N:")
+    if buy_photo == "Y":
+     bill += 3
+
+     print(f"Your total bill is {bill}")
+
+else:
+ print("Sorry you're too small to ride the Cruising Coaster!")
