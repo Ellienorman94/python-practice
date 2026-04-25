bb2b94d673fe0fdd6233bf7a6a3cbf4dc173e7b6
Ellie Norman <eleanornorman9@gmail.com>
2026-04-25 12:29:17 +0100

    Rock, Paper Scissors game

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,6 +1,71 @@
-import random
-heads_or_tails= random.randint(1,2)
-if heads_or_tails == 1:
-    print("Heads")
+rock = '''
+_______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+paper = '''
+  _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+scissors = ''' 
+   _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+print ("Lets play Rock, Paper, Scissors!")
+
+question = input("rock, paper, scissors?: ")
+
+if question == "rock":
+ print(rock)
+elif question == "paper":
+ print(paper)
+elif question == "scissors":
+ print(scissors)
 else:
-    print("tails")
+ print ("Invalid choice")
+
+import random
+decision = ["rock", "paper", "scissors"]
+random_index= random.randint(0,2)
+if random_index == 0:
+ print(rock)
+elif random_index == 1:
+ print(paper)
+else:
+    print(scissors)
+
+computer_choice = decision[random_index]
+
+if question == computer_choice:
+    print("It's a draw!")
+elif question == "rock" and computer_choice == "scissors":
+    print("You win!")
+elif question == "paper" and computer_choice == "rock":
+    print("You win!")
+elif question == "scissors" and computer_choice == "paper":
+    print("You win!")
+else:
+    print("You lose!")
+
+
+
+
+
+
+
+
+
+
---
+++ WhoPaysTheBill.py
@@ -0,0 +1,19 @@
+friends = ["Alice", "Bob", "Charlie", "David", "Emma"]
+
+import random
+friend_name = random.randint(1, 50)
+
+if friend_name <= 10:
+    print("Alice")
+elif friend_name <= 20:
+    print("Bob")
+elif friend_name <= 30:
+    print("Charlie")
+elif friend_name <= 40:
+    print("David")
+else:
+    print("Emma")
+    
+
+
+
